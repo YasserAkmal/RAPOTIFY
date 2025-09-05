c1e807bd0e7e7beec282206066edbeb643373aa7
YasserAkmal <kitayangbercerita@gmail.com>
2025-09-06 00:44:02 +0700

    update

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { apiGet } from "@/lib/spotify";
 
 export async function GET(req: NextRequest) {
+  const accessTest = req.cookies.get("spotify_access_token");
+  console.log("Access token from accessTest:", accessTest);
   try {
-    const accessTest = req.cookies.get("spotify_access_token");
     const access = req.cookies.get("spotify_access_token")?.value;
     console.log("Access token from cookie:", access);
-    console.log("Access token from accessTest:", accessTest);
     const exp = Number(req.cookies.get("spotify_expires_at")?.value || 0);
     const now = Math.floor(Date.now() / 1000);
 
